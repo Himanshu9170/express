233cb5174cde451d77f10c490b7c99b7490d73d6
Himanshu Saroj <himanshusaroj.mzp@gmail.com>
2025-08-05 06:31:43 +0530

    feat: add logging functionality with Winston and Morgan
    - Updated package.json and package-lock.json to include `morgan` and `winston` dependencies.
    - Created a logger.js file to configure Winston for logging messages to both console and file (app.log).
    - Added initial log entries to app.log for POST requests related to adding tea.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-//server restart ke liye npm i -D nodemon download in depencies  docs express api
+//server khud se restart ke liye npm i -D nodemon download in depencies  docs express api
 //require('dotenv').config()  dot env website for environment variables agar node ka new version hai to dot env ki jarurat nhi pdti hai pehle se hi rahta hai
+
+
 import 'dotenv/config'
 import express from 'express'
+import logger from './logger.js' //logger for logging requests
+//import logger from './logger.js' // custom logger using winston
+import morgan  from "morgan";
+//import { createLogger, format, transports } from 'winston'; // custom logger using winston
+
+
 
 const app = express()
 const port = process.env.PORT || 3000
+app.use(express.json())
 
+const morganFormat = morgan(':method :url :status :response-time ms');
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 
 // app.get("/",(req,res) => {
@@ -19,12 +44,13 @@ const port = process.env.PORT || 3000
             // })
 
              //crud application
-            app.use(express.json())
             
             let teaData = []
             let nextId = 1
             //add a new tea
             app.post('/teas',(req,res) => {
+             // console.log("POST"); 
+             logger.info("POST request is made to add a new tea")             
     const {name,price} = req.body;
     const newTea = {id: nextId++,name,price}
     teaData.push(newTea)
@@ -33,6 +59,7 @@ const port = process.env.PORT || 3000
 
 //get all tea
 app.get('/teas',(req,res) => {
+ // logger.warn("get all tea")
     res.status(200).send(teaData)
 })
 
@@ -61,6 +88,10 @@ app.put('/teas/:id',(req,res) => {
 
 //delete tea
 app.delete('/teas/:id',(req,res) => {
+  console.log("delete");
+  console.log(req.params.id);
+  
+  
   const index = teaData.findIndex((tea) => tea.id === parseInt(req.params.id))
   if(index === -1){
     return res.status(404).send('tea not found')
